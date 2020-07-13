285b92e8cc5dc981028f3c332be1aace4da979d7
cphsiung <chunpeihs@gmail.com>
2020-07-13 00:31:43 -0400

    UI improvement

--- routes/comments.js
+++ routes/comments.js
@@ -21,7 +21,8 @@ router.post("/", middleware.isLoggedIn, function(req, res){
    //lookup campground using ID
    Campground.findById(req.params.id, function(err, campground){
        if(err){
-           console.log(err);
+           req.flash("error", "Something went wrong");
+		   console.log(err);
            res.redirect("/campgrounds");
        } else {
         Comment.create(req.body.comment, function(err, comment){
@@ -35,7 +36,8 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 			   comment.save();
                campground.comments.push(comment);
                campground.save();
-               res.redirect('/campgrounds/' + campground._id);
+               req.flash("success", "Successfully added comment");
+			   res.redirect('/campgrounds/' + campground._id);
            }
         });
        }
@@ -44,12 +46,18 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 
 // COMMENT EDIT ROUTE
 router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
-	Comment.findById(req.params.comment_id, function(err, foundComment){
-		if(err){
-			res.redirect("back");
-		} else {
-			res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err || !foundCampground){
+			req.flash("error", "Campground not found");
+			return res.redirect("back");
 		}
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err){
+				res.redirect("back");
+			} else {
+				res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+			}
+		});
 	});
 });
 
@@ -70,6 +78,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
 		if(err){
 			res.redirect("back");
 		} else {
+			req.flash("success", "Comment deleted");
 			res.redirect("/campgrounds/" + req.params.id);
 		}
 	});
